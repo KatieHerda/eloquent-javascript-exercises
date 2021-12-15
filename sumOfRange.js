dc2dc3fd968513a294d366b3116921d606dde5d7
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-12-15 14:07:01 -0700

    started range exercise.

---
+++ sumOfRange.js
@@ -0,0 +1,5 @@
+//Write a range function that takes two arguments start to end and returns an array containing all the numbers from start up and including end.
+
+const range = function(start, end) {
+
+}
