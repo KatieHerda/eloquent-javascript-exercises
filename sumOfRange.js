3fcdcf4ed71dce3d0b50da3ab218bb9dfbfb77d4
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-12-15 14:11:59 -0700

    declared initial empty array to hold final numbers. initialized a c style loop to obtain these numbers.

--- sumOfRange.js
+++ sumOfRange.js
@@ -1,5 +1,10 @@
 //Write a range function that takes two arguments start to end and returns an array containing all the numbers from start up and including end.
 
 const range = function(start, end) {
-
+  let rangeArr = [];
+  for (let i = start; i <= end; i++) {
+    
+  }
 }
+
+console.log(range(1, 5))
