76ee506d37ac4162b30973f946aa0ee9a53014eb
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-12-15 14:20:29 -0700

    initialized a variable (finalSum) at zero which all numbers in array will be added to.

--- sumOfRange.js
+++ sumOfRange.js
@@ -8,10 +8,15 @@ const range = function(start, end) {
   return rangeArr;
 }
 
-console.log(range(1, 5))
+// console.log(range(1, 5))
 
 // Write a sum function that takes an array of numbers and returns the sum of these numbers.
 
 const sumOfRange = function(arr) {
+  let finalSum = 0;
+  for (let i = 0; i <= arr.length; i++) {
   
+  }
 }
+
+console.log(sumOfRange([1, 2, 3, 4, 5]))
