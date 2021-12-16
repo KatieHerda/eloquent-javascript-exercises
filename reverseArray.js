b4dea5a3d303fbc521f0f5f1f9387048efb0470e
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-12-15 21:00:08 -0700

    used c style loop to loop through array in the reverse order.

--- reverseArray.js
+++ reverseArray.js
@@ -1,5 +1,11 @@
 // Write a function that takes an array as an argument and produces a new array that has the same elements in the inverse order.
 
-const reverseArray = function() {
-  
+const arr = [1, 2, 3, 4, 5];
+
+const reverseArray = function(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    console.log(i)
+  }
 }
+
+reverseArray(arr);
