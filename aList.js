c1103967b2e9affabc396ab86d48bb5f05bd32a3
Ronald Dewer Torres Villamil <82120579+d3w3r@users.noreply.github.com>
2021-12-27 20:48:35 -0500

    Update aList.js
    I don't if you know how to do a list, so this is my help if you need it.

--- aList.js
+++ aList.js
@@ -2,7 +2,16 @@
 const arr = [1, 2, 3]
 
 const arrayToList = function(arr) {
-
-  let result = {};
-
+  //first you have to know the structure of a list is this {value:whatevervalue, next:otherlist}
+  let result = {value:undefined, next:undefined};// this is the begin of the list a have never lose it
+  let pointer = result; // This binding points to the same value that result.
+  
+  for (let val of arr) {// This loop iterates for each element inside the array and extracts the value first 1, then 2, and so on.
+    pointer.value = val;// pointer.value == undefined -> pointer.value == 1 at first iteration
+    pointer.next = {value:undefined, next:undefined}; //Create a new list inside the pointer.next before its value was undefined.
+    pointer = pointer.next;// now change the pointer for the next list, for the next iteration, and the result is waiting while the pointer creates the whole list.
+  }
+  console.log(result);
 }
+
+arrayToList(arr);
